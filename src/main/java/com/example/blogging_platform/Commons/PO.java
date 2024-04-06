588215d4400b3ec736b0c0cac39be74fb08ccf2b
Nzovia <nicholas.maundu@roamtech.com>
2024-04-06 17:58:08 +0300

    setup (Models) added all empty models for initial setup

---
+++ src/main/java/com/example/blogging_platform/Commons/PO.java
@@ -0,0 +1,10 @@
+package com.example.blogging_platform.Commons;
+
+/**
+ * @author Nicholas Nzovia
+ * @On 06/04/2024
+ * @Contact: itsdevelopernic22@gmail.com
+ */
+
+public interface PO {
+}
---
+++ src/main/java/com/example/blogging_platform/models/BlogPost.java
@@ -0,0 +1,10 @@
+package com.example.blogging_platform.models;
+
+/**
+ * @author Nicholas Nzovia
+ * @On 06/04/2024
+ * @Contact: itsdevelopernic22@gmail.com
+ */
+
+public class BlogPost {
+}

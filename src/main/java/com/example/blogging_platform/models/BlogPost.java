0ca7ceb35d6228a6c3d923ef62d3a9b59724f74a
Nzovia <nicholas.maundu@roamtech.com>
2024-04-08 12:48:18 +0300

    setup (validation) added hibernate validator dependency and validation of attributes in BlogPost and SystemUser models respectively

--- src/main/java/com/example/blogging_platform/models/BlogPost.java
+++ src/main/java/com/example/blogging_platform/models/BlogPost.java
@@ -1,10 +1,36 @@
 package com.example.blogging_platform.models;
 
+import com.example.blogging_platform.Commons.PO;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+
 /**
  * @author Nicholas Nzovia
  * @On 06/04/2024
  * @Contact: itsdevelopernic22@gmail.com
  */
 
-public class BlogPost {
+@Entity
+@Table(name = "blog_posts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BlogPost extends PO {
+    @Column(name = "blog_title")
+    @NotEmpty(message = "title required")
+    private String blogTitle;
+    @Column(name = "blog_body")
+    @NotEmpty(message = "description required")
+    private String blogDescription;
+
 }
--- src/main/java/com/example/blogging_platform/models/SystemUser.java
+++ src/main/java/com/example/blogging_platform/models/SystemUser.java
@@ -1,10 +1,38 @@
 package com.example.blogging_platform.models;
 
+import com.example.blogging_platform.Commons.PO;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @author Nicholas Nzovia
  * @On 06/04/2024
  * @Contact: itsdevelopernic22@gmail.com
  */
 
-public class SystemUser {
+@Entity
+@Table(name = "system_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SystemUser extends PO {
+    @Column(name = "first_name")
+    @NotEmpty(message = "first name required")
+    private String FirstName;
+    @Column(name = "last_name")
+    @NotEmpty(message = "last name required")
+    private String lastName;
+    private String userName;
+    @Column(name = "user_email")
+    @NotEmpty(message = "user email required")
+    private String userEmail;
+
+
 }

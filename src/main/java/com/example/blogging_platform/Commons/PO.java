d21b6c73e892e2c9f1271cc688dbf028858ff2e7
Nzovia <nicholas.maundu@roamtech.com>
2024-04-08 10:29:14 +0300

    set_up (parent object PO) added parent class from which all classes will inherit common attributes

--- src/main/java/com/example/blogging_platform/Commons/PO.java
+++ src/main/java/com/example/blogging_platform/Commons/PO.java
@@ -1,10 +1,33 @@
 package com.example.blogging_platform.Commons;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
 /**
  * @author Nicholas Nzovia
  * @On 06/04/2024
  * @Contact: itsdevelopernic22@gmail.com
  */
 
-public interface PO {
+@MappedSuperclass
+public class PO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+     private Long id;
+    @Column(name = "created_by")
+    private String createdBy;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_by")
+    private LocalDateTime updatedBy;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    @Column(name = "deleted_by")
+    private LocalDateTime deletedBy;
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+
+
 }

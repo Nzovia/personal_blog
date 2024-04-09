8e2365a9bed86275842b677bd1727ede0df16be1
Nzovia <nicholas.maundu@roamtech.com>
2024-04-09 23:36:47 +0300

    setup (roles) added roles entity

---
+++ src/main/java/com/example/blogging_platform/models/Role.java
@@ -0,0 +1,30 @@
+package com.example.blogging_platform.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author Nicholas Nzovia
+ * @On 09/04/2024
+ * @Contact: itsdevelopernic22@gmail.com
+ */
+
+@Entity
+@Table(name = "tbl_roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "role_uuid", nullable = false)
+    private String uuid;
+    @Column(name = "role_name", nullable = false)
+    private String roleName;
+
+}

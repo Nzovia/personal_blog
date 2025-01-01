cb4243f36d1942d50ce26ef753f88ca09377da96
Nzovia <nickiemaundu19@gmail.com>
2025-01-02 00:00:04 +0300

    Blog vistor service

---
+++ src/main/java/com/example/blogging_platform/services/implementation/BlogVisitorServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.blogging_platform.services.implementation;
+
+import com.example.blogging_platform.dtos.BlogCommentsRequest;
+import com.example.blogging_platform.dtos.BlogVisitorRequest;
+import com.example.blogging_platform.services.interfaces.BlogVisitorService;
+
+public class BlogVisitorServiceImpl implements BlogVisitorService {
+    @Override
+    public void createAccount(BlogVisitorRequest blogVisitorRequest) {
+
+    }
+
+    @Override
+    public void subscribeToBlogMailAlerts(String email) {
+
+    }
+
+    @Override
+    public void addCommentOnABlog(String blog_uuid, BlogCommentsRequest blogCommentsRequest) {
+
+    }
+}
